e96a94520f6c803facc5086d22aeaf337cae6828
Shota Izumi <syouta_izumi@post.pioneer.co.jp>
2017-09-11 12:07:19 +0900

    fix setup script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# ディレクトリなのでシンボリックリンク生成前に削除しておく。
-if [ -e ~/.vim ] ; then
-	rm -rf ~/.vim
-fi
-ln -sf $(pwd)/.vim ~
-
 ln -sf $(pwd)/.screenrc ~
 ln -sf $(pwd)/.bashrc ~
+ln -sf $(pwd)/.bash_aliases ~
 ln -sf $(pwd)/.gitconfig ~
 ln -sf $(pwd)/.gitattributes ~
-ln -sf $(pwd)/.vimrc ~
